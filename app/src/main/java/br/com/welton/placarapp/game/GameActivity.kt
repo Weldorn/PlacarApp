3dc73233845471112c96f18252c5b765ce10ea0d
Welton Dornelas <dornelas.mag@gmail.com>
2020-09-08 22:19:02 -0300

    Initial commit

---
+++ app/src/main/java/br/com/welton/placarapp/game/GameActivity.kt
@@ -0,0 +1,12 @@
+package br.com.welton.placarapp.game
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.welton.placarapp.R
+
+class GameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+    }
+}
---
+++ app/src/main/java/br/com/welton/placarapp/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package br.com.welton.placarapp.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.welton.placarapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
